c498a5728ece8f7287d0b07f71955e232ba60b67
Giselelasserre <giseleslasserre@gmail.com>
2017-08-21 18:05:16 -0300

    Seed 2 items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Creating items..... "
+
+Item.create! ({
+item_name: "blouse"
+size: "M"
+price: 100
+photo_item:
+days_delivery: 5
+})
+
+
+Item.create! ({
+item_name: "pants"
+size: "L"
+price: 300
+photo_item:
+days_delivery: 3
+})
+
+puts 'Finished the seeding!'
