8757e869af46f97609795f9bdf96919a58f037f0
Giselelasserre <giseleslasserre@gmail.com>
2017-08-25 15:34:55 -0300

    arrows and payment page-fake

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
 
   get "/my_items", to: "items#my_items"
 
-
+  get "/payment", to: "purchases#payment"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
