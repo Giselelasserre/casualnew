b6d3668d41dd5d748bad880a80aaf5bc9dcaa6ec
Giselelasserre <giseleslasserre@gmail.com>
2017-08-26 20:07:05 -0300

    new homepage and footer responsive

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 end
+
+
+# if __FILE__ == $PROGRAM_NAME
+
+# endclass ApplicationController < ActionController::Base
+#   protect_from_forgery with: :exception
+#   before_action :authenticate_user!
+# end
