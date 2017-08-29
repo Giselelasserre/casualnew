0d8fe5bf43deb04bf1f51951855dd3d5601cb3a7
Giselelasserre <giseleslasserre@gmail.com>
2017-08-29 12:47:58 -0300

    review fixed

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   CATEGORIES= ["Blouse", "Shirt", "Accessories", "Pant", "Jacket", "Other"]
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   has_many :purchases, dependent: :destroy
+  has_many :reviews
   has_many :customers, through: :purchases, class_name:"User", source: "user"
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   geocoded_by :address
